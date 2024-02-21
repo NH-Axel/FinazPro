ecb05710b6525b585b5de37ca3a9614fe9125fca
AXEL ENRIQUE NEVARES HERNANDEZ <L22211914@tijuana.tecnm.mx>
2024-02-20 20:56:25 -0800

    Con codigo

--- FinazPro/Form1.Designer.cs
+++ FinazPro/Form1.Designer.cs
@@ -35,10 +35,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(907, 657);
+            this.ClientSize = new System.Drawing.Size(1261, 657);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "FinazPro 0.01v";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- FinazPro/Form1.cs
+++ FinazPro/Form1.cs
@@ -16,5 +16,10 @@ namespace FinazPro
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
